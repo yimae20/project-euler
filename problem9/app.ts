50c1497af348858a4ef4c2ab131e9731379ed282
yimae20 <yimae@minebeamitsumi.com>
2020-04-24 12:59:34 +0900

    node_modules削除、problem9修正

--- problem9/app.ts
+++ problem9/app.ts
@@ -1,22 +1,22 @@
 'use strict';
 
 // const aloopLimit = Math.floor(number/3)-1;
-// let bloopLimit = Math.floor((number - a)/2-1);
+// let bloopLimit = Math.floor((number - a)/2);
 
-function findPythagoreanTriplet(number): number[] {
-  for (let a = 1; a <= Math.floor(number / 3) - 1; a++) {
-    for (let b = a + 1; b <= Math.floor((number - a) / 2 - 1); b++) {
-      const c = number - a - b;
+function findPythagoreanTriplet(abcSum: number): { a: number; b: number; c: number } {
+  for (let a = 1; a <= Math.floor(abcSum / 3) - 1; a++) {
+    for (let b = a + 1; b <= Math.floor((abcSum - a) / 2); b++) {
+      const c = abcSum - a - b;
       if (Math.pow(c, 2) === Math.pow(a, 2) + Math.pow(b, 2)) {
         console.log(`a:${a}, b:${b}, c:${c}`);
-        return [a, b, c];
+        return { a, b, c };
       }
     }
   }
-  throw `a + b + c = ${number}となるピタゴラスの三重項はありません。`;
+  throw Error(`a + b + c = ${absSum}となるピタゴラスの三重項はありません。`);
 }
 
-const number = 1000;
-const pythagoreanTriplet = findPythagoreanTriplet(number);
-const answer = pythagoreanTriplet[0] * pythagoreanTriplet[1] * pythagoreanTriplet[2];
-console.log(`a + b + c = ${number}となるピタゴラスの三重項の積abcは ${answer} です。`);
+const absSum = 1000;
+const pythagoreanTriplet = findPythagoreanTriplet(absSum);
+const answer = pythagoreanTriplet.a * pythagoreanTriplet.b * pythagoreanTriplet.c;
+console.log(`a + b + c = ${absSum}となるピタゴラスの三重項の積abcは ${answer} です。`);
