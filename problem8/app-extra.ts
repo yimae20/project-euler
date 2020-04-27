967a0313d76c1619651b1326ea6bbafd61a18c53
yimae20 <yimae@minebeamitsumi.com>
2020-04-27 13:34:48 +0900

    problem8の発展課題app-extra.tsを修正

--- problem8/app-extra.ts
+++ problem8/app-extra.ts
@@ -1,25 +1,35 @@
 'use strict';
 import * as fs from 'fs';
 
-function greatestProduct(fileName: string, digit: number): number {
-  const number = fs.readFileSync(fileName, 'utf-8');
-  const arrayDigitNumber: number[] = number
-    .split('')
-    .filter((value) => value !== '\r' && value !== '\n')
-    .map((value) => parseInt(value, 10));
+const fileName = './digit-number.txt';
+const digit = 13;
 
+new Promise((resolve, rejects) => {
+  fs.readFile(fileName, 'utf-8', (err, data) => {
+    err ? rejects(err) : resolve(data); //if (err) {rejects(err)} else {resolve(data)}
+  });
+})
+  .then((data: string) => {
+    const arrayDigitNumber = data
+      .split('')
+      .filter((value) => value !== '\r' && value !== '\n')
+      .map((value) => parseInt(value, 10));
+    return arrayDigitNumber;
+  })
+  .then((arrayDigitNumber) => {
+    const answer = getGreatestProduct(arrayDigitNumber);
+    console.log(`隣接する13桁の数字のうち、積の最も大きいものは ${answer} です。`);
+  });
+
+function getGreatestProduct(arrayDigitNumber: number[]): number {
   //すべての隣接する指定桁の数字の積を計算し、最大値をanswerに代入する。（改善できるかも・・・）
   const maxArray = arrayDigitNumber.length - digit;
   let answer = 0;
   for (let i = 0; i <= maxArray; i++) {
     const multiNumber = arrayDigitNumber.slice(i, i + digit).reduce((prev, current) => prev * current, 1);
-    // }
-    // console.log(multiNumber);
     if (answer < multiNumber) {
       answer = multiNumber;
     }
   }
   return answer;
 }
-
-console.log(`隣接する13桁の数字のうち、積の最も大きいものは ${greatestProduct('./digit-number.txt', 13)} です。`);
