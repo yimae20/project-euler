b798e4c6da75f2b7933b3d595d9be76f8bff8639
yimae20 <yimae@minebeamitsumi.com>
2020-05-11 09:49:02 +0900

    problem14の修正

--- problem14/app.ts
+++ problem14/app.ts
@@ -1,3 +1,5 @@
+const startTime = Date.now();
+
 function iterativeSequence(number: number): number {
   if (number % 2 === 0) {
     return number / 2;
@@ -10,51 +12,55 @@ function isAlreadyCalcNumber(arrayNumber: number[], targetNumber: number): boole
   return arrayNumber.some((arrayValue) => targetNumber === arrayValue);
 }
 
-//この方法だと、開始数10万以下でも計算時間が数分かかるので改良が必要
+//  この方法だと、開始数10万以下でも計算時間が数分かかる
+//  100万以下で2～3時間かかる
+function findLongestChainNumber(limitNumber: number): number {
+  const arrayCalculatedNumber: number[] = []; //計算した数値の配列
+  const arrayCount: number[] = []; //計算した数値に対応する連鎖回数の配列
+  let longestChainCount = 0;
+  let longestChainNumber = 0;
 
-const limitNumber = 13; //最大開始数および、シーケンス中の最大数値
-const checkNumber: number[] = []; //計算した数値の配列
-const arrCnt: number[] = []; //計算した数値に対応する、反復回数の配列
-let longestChain = 0;
-let longestChainNumber = 0;
-for (let i = 2; i <= limitNumber; i++) {
-  let index = 0;
-  let startNumber = i;
-  let count = 0;
-  while (startNumber !== 1) {
-    /*     すでに計算した数値かどうか確認
-    true : (今までの反復回数＋すでに計算した数値の反復回数)をしてwhileを終了
-    false:シーケンスの実行 */
-    if (isAlreadyCalcNumber(checkNumber, startNumber)) {
-      index = checkNumber.findIndex((item) => item === startNumber);
-      count = arrCnt[index] + count;
-      startNumber = 1;
-    } else {
-      checkNumber.push(startNumber);
-      startNumber = iterativeSequence(startNumber);
-      count++;
+  for (let i = 2; i <= limitNumber; i++) {
+    let index = 0;
+    let currentNumber = i;
+    let count = 0;
+    while (currentNumber !== 1) {
+      // 計算したことがある数値かどうか確認
+      // true : (今までの連鎖回数 ＋ 計算したことがある数値の連鎖回数)をcountに代入して現在の開始数の計算(while loop)を終了
+      // false: シーケンスの実行
+      if (isAlreadyCalcNumber(arrayCalculatedNumber, currentNumber)) {
+        index = arrayCalculatedNumber.findIndex((item) => item === currentNumber);
+        count = arrayCount[index] + count;
+        currentNumber = 1;
+      } else {
+        arrayCalculatedNumber.push(currentNumber);
+        currentNumber = iterativeSequence(currentNumber);
+        count++;
+      }
     }
-
-    /*     TODO
-    limitNumberを超えたかどうか、チェックして超えていれば
-    while loopを終了する
-    超えるまでに計算した数値および反復回数をどうするか要検討 */
+    //新規で計算した回数を確認
+    let newCalculatedCount = 0;
+    if (isNaN(arrayCount[index])) {
+      newCalculatedCount = count - 0;
+    } else {
+      newCalculatedCount = count - arrayCount[index];
+    }
+    //配列arrayCountに連鎖回数を代入
+    for (let j = 0; j < newCalculatedCount; j++) {
+      arrayCount.push(count - j);
+    }
+    //最大連鎖回数とその時の開始数を更新
+    if (longestChainCount < count) {
+      longestChainCount = count;
+      longestChainNumber = i;
+    }
   }
-  //すでに計算した数値であれば反復回数を、計算した数値でなければ、0を代入
-  let x = 0;
-  if (isNaN(arrCnt[index])) {
-    x = 0;
-  } else {
-    x = arrCnt[index];
-  }
-  //計算した数値の反復回数を配列arrCntに代入
-  for (let j = 0; j < count - x; j++) {
-    arrCnt.push(count - j);
-  }
-  //反復回数が長ければ、最大反復回数とその時の開始数を更新
-  if (longestChain < count) {
-    longestChain = count;
-    longestChainNumber = i;
-  }
+  // console.log(longestChainNumber, longestChain);
+  return longestChainNumber;
 }
-console.log(longestChainNumber, longestChain);
+
+const limitNumber = 1000; // 開始数の最大値
+console.log(`${limitNumber} 以下の開始数の中で最も長い連鎖ができるのは ${findLongestChainNumber(limitNumber)} です。`);
+
+const endTime = Date.now();
+console.log(`計算時間：${endTime / 1000 - startTime / 1000}秒`);
